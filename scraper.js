79805464e5421d98d994eb8d82848ac053811f9b
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-01 14:36:51 -0500

    added post request to spotify api

--- scraper.js
+++ scraper.js
@@ -18,3 +18,36 @@ const querystring = require('querystring');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+app.post('/spotify', async (req, res) => {
+    const { username, password } = req.body;
+    const token_url = 'https://accounts.spotify.com/api/token';
+    const auth_token = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+  
+    try {
+      const response = await axios.post(token_url, 'grant_type=client_credentials', {
+        headers: {
+          'Authorization': `Basic ${auth_token}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+  
+      const access_token = response.data.access_token;
+      const user_url = 'https://api.spotify.com/v1/me';
+      const user_response = await axios.get(user_url, {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
+      });
+  
+      const user_id = user_response.data.id;
+      console.log(`User ID: ${user_id}`);
+      res.send(`User ID: ${user_id}`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error');
+    }
+  });
+  
+  app.listen(8000, () => {
+    console.log('Server started on port 8000');
+  });
