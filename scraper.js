a1294d8f98967bf5f732cb484a5603546c58f62a
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-01 14:34:48 -0500

    adding app

--- scraper.js
+++ scraper.js
@@ -4,7 +4,17 @@
 
 //Get tracks from the playlist 
 
-
+//Api variables
 require('dotenv').config();
 const clientId = process.env.CLIENTID;
 const clientSecret = process.env.CLIENTSECRET;
+
+//Setup app
+const express = require('express');
+const app = express();
+const request = require('request');
+const querystring = require('querystring');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
